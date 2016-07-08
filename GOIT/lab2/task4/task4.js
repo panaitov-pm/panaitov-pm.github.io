35da7d6da8d60af98fb028e454862619ad143273
QAPasha <qapasha.ua@gmail.com>
2016-07-09 00:40:07 +0300

    del lab2

--- GOIT/lab2/task4/task4.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Web on 28.06.2016.
- */
-var a = 1, b = 2;
-var result;
-(a + b <= 3)? result = 'Yep!': result = 'Noup!';
-console.log(result);
--- GOIT/lab2/task5/task5.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Web on 28.06.2016.
- */
-var name = '' , text;
-
-name == 'admin'? text = 'Hi':
-name == 'manager'? text = 'Hello':
-name == ''? text = 'No login': text = '';
-console.log(text);
